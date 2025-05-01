da125f4baf66d50563038962494eb001b95b5142
T_TAKUMA <T_TAKUMA@LAPTOP-B598CLGF>
2025-05-01 09:53:58 +0900

    千々岩変更②

--- src/scoremanager/main/SubjectListAction.java
+++ src/scoremanager/main/SubjectListAction.java
@@ -14,6 +14,7 @@ import tool.Action;
 public class SubjectListAction extends Action{
 	public void execute(HttpServletRequest req, HttpServletResponse res
 			) throws Exception {
+
 		try {
 			HttpSession session = req.getSession();
 			Teacher teacher = (Teacher)session.getAttribute("user");
